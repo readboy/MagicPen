cd40b1ffc3ba138bfb356999e0a20498af3c2e21
rbGuanJiaYin <710746690@qq.com>
2015-06-15 07:53:38 +0800

    Initial Commit

---
+++ MagicPen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicPen
+//
+//  Created by readboy2 on 15/6/15.
+//  Copyright (c) 2015年 readboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagicPen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicPen
+//
+//  Created by readboy2 on 15/6/15.
+//  Copyright (c) 2015年 readboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
